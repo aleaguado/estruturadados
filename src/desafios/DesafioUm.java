534527ab4950bf4c5d90291c591ad02b86f29870
aleaguado <aleaguado@alenovodeb>
2014-07-31 15:16:01 -0300

    Primeira Versao

---
+++ src/desafios/DesafioUm.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package desafios;
+
+/**
+ *
+ * @author aleaguado
+ */
+import java.util.Scanner;
+
+public class DesafioUm {
+     public static void main(String[] args)
+      {
+          String[] StringVetor = new String[5]; //Instancio o vetor
+          Scanner entrada = new Scanner (System.in); //Instancio o objeto entrada do tipo Scanner
+          boolean teste = false;
+                    
+          System.out.println("Bem Vindo ao Programa de Vetores!!!");
+          System.out.println("Digite o primeiro nome!!!");
+          StringVetor[0] = entrada.nextLine();
+          System.out.println("Digite o segundo nome!!!");
+          StringVetor[1] = entrada.nextLine();
+          System.out.println("Digite o terceiro nome!!!");
+          StringVetor[2] = entrada.nextLine();
+          System.out.println("Digite o quarto nome!!!");
+          StringVetor[3] = entrada.nextLine();
+          System.out.println("Digite o quinto nome!!!");
+          StringVetor[4] = entrada.nextLine();
+
+          System.out.println("Muito Bem! Agora que você cadastrou os nomes, digite uma letra");
+          String busca = entrada.nextLine(); //Pegar c/ o usuario a letra da busca
+          
+          for (int i = 0; i < 5; i++) {   //Varemos o vetor neste for ... 
+             
+            if (StringVetor[i].charAt(0) == busca.charAt(0)) { //Comparamos o primeiro caracter 
+                System.out.println("O nome " + StringVetor[i] + " começa com a letra " + busca ); 
+                teste = true;
+            }
+            }
+          
+           if (!teste) { //Se não houver nenhum nome c/ a letra, imprimimos isso ...
+            System.out.println("Nenhum nome começa com a letra: " + busca);
+           }
+                  
+      }  // termina o metodo main
+    
+}
---
+++ src/topico1_vetores/vetorprocedimental.java
@@ -0,0 +1,62 @@
+package topico1_vetores;
+// vetorprocedimental.java
+// demonstra a utilização de vetores em java
+////////////////////////////////////////////////////////////////
+class VetorApp
+   {
+   public static void main(String[] args)
+      {
+      long[] vet;                  // referencia para vetor
+      vet = new long[100];         // cria o vetor
+      int nElems = 0;              // número de itens
+      int j;                       // contador dos laços
+      long chaveBusca;              // chave de Busca
+//--------------------------------------------------------------
+//Rotina de inserção ....
+//--------------------------------------------------------------
+      vet[0] = 77;                 // insere 10 itens
+      vet[1] = 99;
+      vet[2] = 44;
+      vet[3] = 55;
+      vet[4] = 22;
+      vet[5] = 88;
+      vet[6] = 11;
+      vet[7] = 00;
+      vet[8] = 66;
+      vet[9] = 33;
+      nElems = 10;                 // insere a quantidade de itens no vetor na variavel nElems
+//--------------------------------------------------------------
+//Rotina para visualizar os itens do vetor ....
+//--------------------------------------------------------------
+      for(j=0; j<nElems; j++)      // mostra os itens do vetor
+            System.out.print(vet[j] + " ");
+      System.out.println("");
+//--------------------------------------------------------------
+//Rotina de busca ....
+//--------------------------------------------------------------
+      chaveBusca = 66;              // seta a busca para achar o valor 66
+      for(j=0; j<nElems; j++)          // faz o for varrer o vetor
+         if(vet[j] == chaveBusca)       // compara ... encontrou?
+            break;                     // ...se encontrou termina o for antes de finalizar..
+      if(j == nElems)                  // chegou ao final do vetor?
+         System.out.println("Não localizei o item " + chaveBusca); // sim
+      else
+         System.out.println("Encontrei o item " + chaveBusca);      // não
+//--------------------------------------------------------------
+//Rotina de remoção ....
+//--------------------------------------------------------------
+      chaveBusca = 55;              // seta a busca e remoção p/ o valor 55
+      for(j=0; j<nElems; j++)           // faz o for p/ varrer o vetor
+      if(vet[j] == chaveBusca)          // se encontrar o numero 55 ...
+         break;                         // para a busca no ponto que encontrou
+      for(int k=j; k<nElems; k++)       // e através do for, a partir do local que encontrou..
+         vet[k] = vet[k+1];             // move todos itens (uma posição pra frente)
+      nElems--;                         // decrementa a variavel nElems
+//--------------------------------------------------------------
+//Rotina de visualização ....
+//--------------------------------------------------------------
+      for(j=0; j<nElems; j++)      // mostra novamente os itens
+         System.out.print( vet[j] + " ");
+      System.out.println("");
+      }  // finaliza o método main
+   }  // finaliza a classe VetorApp
